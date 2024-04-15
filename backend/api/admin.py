b39695385ec850b52aca80152d7f213b24fc8965
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2024-04-15 14:48:25 +0500

    created models and serializers

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import User, Tweet, Comment, Like, Followers
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Tweet)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Followers)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pfp = models.CharField()
+    bio = models.TextField()
+
+    class Meta:
+        app_label = "api"
+
+
+class Tweet(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.Cascade)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        app_label = "api"
+
+
+class Comment(models.Model):
+    tweet_id = models.ForeignKey(Tweet, on_delete=models.Cascade)
+    user_id = models.ForeignKey(User, on_delete=models.Cascade)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        app_label = "api"
+
+
+class Like(models.Model):
+    post_id = models.ForeignKey(Tweet, on_delete=models.Cascade)
+    user_id = models.ForeignKey(User, on_delete=models.Cascade)
+
+    class Meta:
+        app_label = "api"
+        unique_together = ["tweet_id", "user_id"]
+
+
+class Followers(models.Model):
+    followed_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    follower_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = "api"
+        unique_together = ["followed_id", "follower_id"]
