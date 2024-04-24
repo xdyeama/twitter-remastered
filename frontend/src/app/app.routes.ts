d2218d4dae7ec3bb29ab0587eb6d3fcaf3150b5a
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2024-04-24 15:22:24 +0500

    added followers list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { TweetDetailComponent } from './tweet-detail/tweet-detail.component';
+import { FollowersListComponent } from './followers-list/followers-list.component';
 
 export const routes: Routes = [
     { path: '', component: ReactiveFormComponent },
     { path: ':username', component: UserPageComponent, title: 'User page' },
+    { path: ':username/followers', component: FollowersListComponent, title: 'User followers' },
     { path: 'tweets/:id', component: TweetDetailComponent, title: 'Tweet' },
 ];
