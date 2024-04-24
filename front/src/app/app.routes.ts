00b82763e2e45761697c28a314fc826bd81fdca2
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2024-04-24 14:34:48 +0500

    Delete front directory

--- front/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { FeedComponent } from './feed/feed.component';
-
-export const routes: Routes = [
-    { path: 'login', component: LoginFormComponent},
-    { path: 'feed', component: FeedComponent},
-];
